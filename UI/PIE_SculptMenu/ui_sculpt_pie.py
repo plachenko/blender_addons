5268373f8513499175cad23cd01623aaacd868c0
denis <plachenko@gmail.com>
2016-06-18 23:35:08 -0400

    what isnt a waste of time...?

--- UI/PIE_SculptMenu/ui_sculpt_pie.py
+++ UI/PIE_SculptMenu/ui_sculpt_pie.py
@@ -249,7 +249,6 @@ class VIEW3D_PIE_sculpt(Menu):
         topL_row.prop(sculpt, "use_symmetry_y", text="mir Y", toggle=True)
         topL_row.prop(sculpt, "tile_y", text="tile Y", toggle=True)
         topL_row.prop(sculpt, "lock_y", text="lock Y", toggle=True)
-
         
 def register():
     bpy.utils.register_class(VIEW3D_PIE_sculpt)
